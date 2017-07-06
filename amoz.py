a9eef0b6bb7546cb01861dedf76e1edd5e82c268
MGH <aali361@gmail.com>
2017-07-06 17:11:36 +0430

    Getting Gpa & GetUnits & PassUnits of All Terms

--- amoz.py
+++ amoz.py
@@ -1,9 +1,12 @@
+# -*- coding: utf-8 -*-
 import requests
+import urllib3
 from PIL import Image
 from io import BytesIO
 from bs4 import BeautifulSoup
-import re
+# import re
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 url_captcha="http://golestan.araku.ac.ir/home/balancer/balancer.aspx?vv=2&cost=main"
 url_captcha_img="http://golestan.araku.ac.ir/home/balancer/captcha.aspx"
@@ -12,18 +15,17 @@ url_set_cid="https://golestan.araku.ac.ir/Forms/AuthenticateUser/setcid.aspx?cid
 s=requests.session()
 r=s.get(url_captcha)
 
-img_raw=s.get(url_captcha_img)
-img_byte=BytesIO(img_raw.content)
-img=Image.open(img_byte)
-img.save('captcha.jpg')
-
-
 soup = BeautifulSoup(r.text, 'html.parser')
 
 VIEWSTATE=soup.find(id="__VIEWSTATE").get('value')
 VIEWSTATEGENERATOR=soup.find(id="__VIEWSTATEGENERATOR").get('value')
 EVENTVALIDATION=soup.find(id="__EVENTVALIDATION").get('value')
 
+# print(r.cookies['ASP.NET_SessionId'])
+img_raw=s.get(url_captcha_img)
+img_byte=BytesIO(img_raw.content)
+img=Image.open(img_byte)
+img.save('captcha.jpg')
 
 s.headers.update({'Referer': 'http://golestan.araku.ac.ir/home/balancer/balancer.aspx?vv=2&cost=main'})
 s.headers.update({'Accept': 'text/html, application/xhtml+xml, */*'})
@@ -95,6 +97,15 @@ start=seq.find(str.encode(lt+"','"+lt))+len(lt+"','"+lt)
 end=seq.find(str.encode('ورود به سيستم'))
 seq=seq[start+2:end-2]
 seq=seq.decode()
+
+name=r.content[end:]
+start=name.find(str.encode("SetUsr('"))+len("SetUsr('")
+end=name.find(str.encode("afterlog"))
+name=name[start-1:end-3]
+name=name.decode("utf-8", "ignore")
+print(name)
+
+# exit()
 
 s.cookies.update({'seq':seq,'su':'0','f':'1','ft':'0'})
 s.headers.update({'Referer': 'https://golestan.araku.ac.ir/Forms/AuthenticateUser/main.htm'})
@@ -178,7 +189,7 @@ VIEWSTATEGENERATOR=soup.find(id="__VIEWSTATEGENERATOR").get('value')
 EVENTVALIDATION=soup.find(id="__EVENTVALIDATION").get('value')
 TicketTextBox=soup.find(id="TicketTextBox").get('value')
 
-#
+#Getting Ettelaat Jamme
 s.cookies.update({'seq':seq,'sno':'9213231259','stdno':'9213231259','su':'3','f':'12310','ft':'0'})
 url='https://golestan.araku.ac.ir/Forms/F1802_PROCESS_MNG_STDJAMEHMON/F1802_01_PROCESS_MNG_STDJAMEHMON_Dat.aspx?r=0.6707888345168351&fid=0%3b12310&b=0&l=0&tck='+lt+'&&lastm=20140622082604'
 r=s.post(url,data={'__VIEWSTATE':VIEWSTATE,'__VIEWSTATEGENERATOR':'6AC8DB9B',
@@ -186,7 +197,79 @@ r=s.post(url,data={'__VIEWSTATE':VIEWSTATE,'__VIEWSTATEGENERATOR':'6AC8DB9B',
                                         'Fm_Action':'08','Frm_Type':'','Frm_No':'','TicketTextBox':TicketTextBox,'XMLStdHlp':'','TxtMiddle':'<r F41251="9213231259"/>'})
 
 
+content=r.content
 
+# print(content)
+
+start=content.find(str.encode('F17551'))
+start=start+len('F17551')
+end=content.find(str.encode('F41301'))
+Field=content[start+2:end-2]
+Field=Field.decode()
+print(Field)
+
+start=content.find(str.encode('F41701'))
+start=start+len('F41701')
+end=content.find(str.encode('F42191'))
+Gpa=content[start+1:end-1]
+Gpa=Gpa.decode()
+print(Gpa)
+
+start=content.find(str.encode('F41801'))
+start=start+len('F41801')
+end=content.find(str.encode('F42251'))
+Punits=content[start+1:end-1]
+Punits=Punits.decode()
+
+print(Punits)
+
+
+content=content[end:]
+term=content.find(str.encode('F4350="'))
+start=term
+while start != -1:
+    start=start+len('F4350="')
+    end=content.find(str.encode('" F4455'))
+    term=content[start:end]
+    term=term.decode()
+    print(term)
+    end=end+len('" F4455')
+    content=content[end+1:]
+
+    start = content.find(str.encode('F4365="'))
+    start = start + len('F4365="')
+    end = content.find(str.encode('" CumGet'))
+    tUnitsGet = content[start:end]
+    tUnitsGet = tUnitsGet.decode()
+    print(tUnitsGet)
+    end = end + len('" CumGet')
+    content = content[end + 1:]
+
+    start = content.find(str.encode('F4370="'))
+    start = start + len('F4370="')
+    end = content.find(str.encode('" F4180'))
+    tUnitsPass = content[start:end]
+    tUnitsPass = tUnitsPass.decode()
+    print(tUnitsPass)
+    end = end + len('" F4180')
+    content = content[end + 1:]
+
+    start = content.find(str.encode('F4360="'))
+    start = start + len('F4360="')
+    end = content.find(str.encode('" CumGPA'))
+    tGpa = content[start:end]
+    tGpa = tGpa.decode()
+    print(tGpa)
+    end = end + len('" CumGPA')
+    content = content[end + 1:]
+
+
+    term = content.find(str.encode('F4350="'))
+    start=term
+
+
+
+exit()
 
 soup = BeautifulSoup(r.text, 'html.parser')
 VIEWSTATE=soup.find(id="__VIEWSTATE").get('value')
@@ -232,5 +315,5 @@ while id != -1:
     stGrOfCourse = stGrOfCourse + len('F3945="')
     enGrOfCourse = content.find(str.encode('" F3955="'))
 
-    print(content[stOfCourse:enOfCourse].decode() + ": " + content[stGrOfCourse:enGrOfCourse].decode())
+    print(content[stOfCourse:enOfCourse].decode("utf-8", "ignore") + ": " + content[stGrOfCourse:enGrOfCourse].decode("utf-8", "ignore"))
     id = content.find(str.encode('id="'))
